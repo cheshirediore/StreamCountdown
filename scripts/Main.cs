d645a4f65b4abbec284f69cf9b1128640936fe9a
Cheshire Diore <cheshirediore@gmail.com>
2024-04-01 14:58:31 -0400

    Change rendering to support transparency

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -7,7 +7,7 @@ public partial class Main : Node2D
 	public override void _Ready()
 	{
 		// GD Script version: DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true)
-		// DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.Transparent, true);
+		DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.Transparent, true);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
