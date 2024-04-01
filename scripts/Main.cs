f8e4a346a49194a8d75a148b5440543b6428d647
Cheshire <cheshirediore@gmail.com>
2024-04-01 14:27:08 -0400

    Working clock milestone

--- scripts/Main.cs
+++ scripts/Main.cs
@@ -1,25 +1,17 @@
 using Godot;
 using System;
 
-namespace Clock
+public partial class Main : Node2D
 {
-public partial class Clock : Control
-{
-	[Export] 
-	private LabelComponent label;
-
-	[Export]
-	private TimerComponent timer;
-
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// GD Script version: DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_TRANSPARENT, true)
+		// DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.Transparent, true);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-
-}
 }
